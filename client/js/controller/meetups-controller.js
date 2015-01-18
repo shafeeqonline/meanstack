9282df5a2cb5246062c250ceb811cfa6c93b9b2c
Shafeeq <shafeeq.rahman01@gmail.com>
2015-01-18 23:59:19 +0530

    Simple list of books with M

--- client/js/controller/meetups-controller.js
+++ client/js/controller/meetups-controller.js
@@ -1,23 +1,17 @@
 app.controller('meetupsController', ['$scope', '$resource', function($scope, $resource){
 	var Meetup = $resource('/api/meetups');
-	$scope.meetups = [
-		{
-			name : 'Shafeeq'
-		},
-		{
-			name : 'Rahul'
-		},
-		{
-			name : 'Pooja'
-		}
-	]
+
+	Meetup.query(function(results){
+		$scope.meetups = results;
+	})
+
+
 	$scope.createMeetup = function(){
-		/*var meetup = new Meetup();
+		var meetup = new Meetup();
 		meetup.name = $scope.meetupName;
-		meetup.$save();*/
-		$scope.meetups.push({ name : $scope.meetupName});
+		meetup.$save(function(result){
+			$scope.meetups.push(result);
+		});
 		$scope.meetupName = '';
-		$scope.meetupsCount=$scope.meetups.length;
 	};
-	$scope.meetupsCount=$scope.meetups.length;
 }])
--- server.js
+++ server.js
@@ -2,20 +2,26 @@ var express = require('express'),
 	app		= express(),
 	path 	= require('path'),
 	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
 	meetupsController = require('./server/controller/meetups-controller');
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '/client/views/index.html'));
 });
 
-app.use(bodyParser());
+mongoose.connect('mongodb://localhost:27017/mean-demo');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.use('/js', express.static(__dirname + '/client/js'));
 
+
+app.get('/api/meetups', meetupsController.list);
 app.post('/api/meetups', meetupsController.create);
 
 app.listen(3000, function(){
-	console.log('I am  now listening');
-	if(express.static)
-		console.log("It does");
+	console.log('I am  now listening on 3000');
 })		 
